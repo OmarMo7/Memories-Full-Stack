7e370de9fb74d49339f7dd2ee55bb5815f03be9f
OmarMo7 <omar7.first@gmail.com>
2022-05-01 00:23:33 +0200

    Committing new changes to server and client folders

--- src/api/index.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const url = 'http://localhost:8000/posts'
-
-export const fetchPosts = () => axios.get(url);
-export const createPost = (post) => axios.post(url, post);
-export const updatePost = (id, post) => axios.patch(`${url}/${id}`, post);
-export const deletePost = (id) => axios.delete(`${url}/${id}`);
-export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
--- src/reducers/index.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from "redux";
-import { posts } from './posts'
-export default combineReducers({
-  posts, //They have the same name 'posts'
-})
