df7fbe5f2605a97305fa25bfc5d0ac9d464993a8
OmarMo7 <omar7.first@gmail.com>
2022-04-30 23:07:56 +0200

    Putting a little margin between like button & count

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -51,7 +51,7 @@ const Post = ({ post, setCurrentId }) => {
         <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
       </CardContent>
       <CardActions className={classes.cardActions}>
-        <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}><ThumbUpAltIcon fontSize="small" /> Like {post.likeCount} </Button>
+        <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}><ThumbUpAltIcon fontSize="small" /> Like &nbsp; {post.likeCount} </Button>
         <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}><DeleteIcon fontSize="small" /> Delete</Button>
       </CardActions>
     </Card>
